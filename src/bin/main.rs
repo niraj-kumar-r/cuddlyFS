246f7cfdfa9a7dfdf5335ca524581bcd88777680
Niraj Kumar <nirajk23523@gmail.com>
2024-08-14 17:21:14 +0530

    included trait in scope

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,11 +1,11 @@
-use cuddlyfs::p2p;
+use cuddlyfs::p2p::{tcp_transport, Transport};
 use tokio;
 
 #[tokio::main]
 async fn main() {
     env_logger::init();
 
-    let tr = p2p::tcp_transport::TcpTransport::new("127.0.0.1:4000");
+    let tr = tcp_transport::TcpTransport::new("127.0.0.1:4000");
     match tr {
         Ok(mut transport) => {
             let _ = transport.listen_and_accept().await;
