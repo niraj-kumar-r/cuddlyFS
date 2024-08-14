df64c7c8414ea1bd23f1169859e62e37832a0f9b
Niraj Kumar <nirajk23523@gmail.com>
2024-08-14 15:22:28 +0530

    added new method to TcpTransport

--- src/p2p/tcp_transport.rs
+++ src/p2p/tcp_transport.rs
@@ -5,4 +5,66 @@ pub struct TcpTransport {
     peers: std::collections::HashMap<std::net::SocketAddr, TcpPeer>,
 }
 
-pub struct TcpPeer {}
+impl TcpTransport {
+    pub fn new(listen_address: &str) -> Result<Self, std::net::AddrParseError> {
+        let listen_address = listen_address.parse::<std::net::SocketAddr>();
+        match listen_address {
+            Ok(listen_address) => Ok(Self {
+                listen_address,
+                listener: None,
+                mu: tokio::sync::Mutex::new(()),
+                peers: std::collections::HashMap::new(),
+            }),
+            Err(e) => {
+                eprintln!("Failed to parse listen address: {:?}", e);
+                Err(e)
+            }
+        }
+    }
+
+    pub async fn listen_and_accept(&mut self) {
+        let listener = tokio::net::TcpListener::bind(&self.listen_address)
+            .await
+            .unwrap();
+        self.listener = Some(listener);
+
+        while let Some(listener) = &self.listener {
+            match listener.accept().await {
+                Ok((socket, addr)) => {
+                    println!("Accepted connection from {:?}", addr);
+                    let peer = TcpPeer::new(socket, addr);
+                    self.peers.insert(addr, peer);
+                }
+                Err(e) => {
+                    eprintln!("Failed to accept connection: {:?}", e);
+                }
+            }
+        }
+    }
+}
+
+pub struct TcpPeer {
+    socket: tokio::net::TcpStream,
+    peer_addr: std::net::SocketAddr,
+}
+
+impl TcpPeer {
+    pub fn new(socket: tokio::net::TcpStream, peer_addr: std::net::SocketAddr) -> Self {
+        Self { socket, peer_addr }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_tcp_transport() {
+        let transport = TcpTransport::new("127.0.0.1:4000");
+        assert!(transport.is_ok());
+
+        // test the address
+        let transport = transport.unwrap();
+        assert_eq!(transport.listen_address, "127.0.0.1:4000".parse().unwrap());
+    }
+}
