03d2da11963487ee0e6e2a384ac529faa0c869c8
Niraj Kumar <nirajk23523@gmail.com>
2024-11-17 23:13:33 +0530

    added methods to interact with files on disk

--- src/datanode/datanode_data_registry.rs
+++ src/datanode/datanode_data_registry.rs
@@ -47,6 +47,43 @@ impl DatanodeDataRegistry {
         Ok(File::from_std(file))
     }
 
+    pub(crate) async fn start_block_creation(&self, block: &Block) -> CuddlyResult<File> {
+        if self.block_exists(block) {
+            return Err(CuddlyError::IOError(format!(
+                "Block {:?} already exists",
+                block
+            )));
+        }
+
+        self.insert_in_progress_block(block)?;
+        let block_path = self.block_directory.join(block.filename() + ".tmp");
+        let file = match File::create(block_path).await {
+            Ok(file) => file,
+            Err(e) => {
+                let mut blocks_being_created = self.blocks_being_created.lock().unwrap();
+                blocks_being_created.deref_mut().remove(block);
+                return Err(e.into());
+            }
+        };
+
+        Ok(file)
+    }
+
+    pub(crate) async fn abort_block_creation(&self, block: &Block) -> CuddlyResult<()> {
+        self.remove_in_progress_block(block)
+    }
+
+    pub(crate) async fn finish_block_creation(&self, block: &Block) -> CuddlyResult<()> {
+        self.remove_in_progress_block(block)?;
+        let filename = self.block_directory.join(block.filename());
+        let temp_filename = self
+            .block_directory
+            .join(format!("{}.tmp", block.filename()));
+        tokio::fs::rename(temp_filename, filename).await?;
+
+        Ok(())
+    }
+
     fn insert_in_progress_block(&self, block: &Block) -> CuddlyResult<()> {
         let mut blocks_being_created = self.blocks_being_created.lock().unwrap();
         if blocks_being_created.deref().contains(block) {
@@ -72,6 +109,9 @@ impl DatanodeDataRegistry {
     }
 
     fn block_exists(&self, block: &Block) -> bool {
-        self.block_directory.join(block.filename()).exists()
+        self.block_directory
+            .join(block.filename())
+            .try_exists()
+            .unwrap_or(false)
     }
 }
