ee6e135c297a3046c59ead40f623d066ecc06d1c
Niraj Kumar <nirajk23523@gmail.com>
2024-11-17 15:13:49 +0530

    disk info refreshes

--- src/datanode/datanode_disk_info.rs
+++ src/datanode/datanode_disk_info.rs
@@ -1,6 +1,6 @@
-use std::{path::PathBuf, time::Duration};
+use std::{path::PathBuf, process::Command};
 
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, Duration, Utc};
 
 use crate::{errors::CuddlyResult, APP_CONFIG};
 
@@ -14,14 +14,48 @@ pub(crate) struct DiskInfo {
 
 impl DiskInfo {
     pub fn new() -> CuddlyResult<Self> {
-        let disk_info = Self {
+        let mut disk_info = Self {
             data_dir: APP_CONFIG.datanode.data_dir.clone(),
             used: 0,
             available: 0,
             last_update: Utc::now(),
-            update_interval: Duration::from_secs(APP_CONFIG.datanode.disk_check_interval),
+            update_interval: Duration::seconds(APP_CONFIG.datanode.disk_check_interval as i64),
         };
-
+        disk_info.refresh(true)?;
         Ok(disk_info)
+    }
+
+    fn refresh(&mut self, force: bool) -> CuddlyResult<()> {
+        if force || self.needs_update() {
+            let output = Command::new("df")
+                .arg("-k")
+                .arg("--output=used,avail")
+                .arg(&self.data_dir)
+                .output()?;
+
+            let output = String::from_utf8_lossy(&output.stdout);
+            let mut lines = output.lines();
+            lines.next();
+            let line = lines.next().unwrap();
+            let mut columns = line.split_whitespace();
+            self.used = columns.next().unwrap().parse().unwrap();
+            self.available = columns.next().unwrap().parse().unwrap();
+            self.last_update = Utc::now();
+        }
+        Ok(())
+    }
+
+    fn needs_update(&self) -> bool {
+        Utc::now().signed_duration_since(self.last_update) > self.update_interval
+    }
+
+    pub(crate) fn get_used(&mut self) -> CuddlyResult<u64> {
+        self.refresh(false)?;
+        Ok(self.used)
+    }
+
+    pub(crate) fn get_available(&mut self) -> CuddlyResult<u64> {
+        self.refresh(false)?;
+        Ok(self.available)
     }
 }
