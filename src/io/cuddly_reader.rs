9c73f3aed343b673c6f590068e90dd7b2e197fa8
Niraj Kumar <nirajk23523@gmail.com>
2024-12-04 00:40:18 +0530

    implemented a simple reader at the moment

--- src/io/cuddly_reader.rs
+++ src/io/cuddly_reader.rs
@@ -1,73 +1,166 @@
-// use std::{future::Future, pin::Pin, task::Poll};
+use std::io::Read;
 
-// use tokio::io::{AsyncRead, BufReader};
+use prost::Message;
+use tokio::io::AsyncReadExt;
+use tokio::io::AsyncWriteExt;
+use tokio::{io::BufStream, net::TcpStream};
 
-// use crate::{
-//     cuddlyproto::{
-//         client_data_node_service_client::ClientDataNodeServiceClient,
-//         file_service_client::FileServiceClient, BlockWithLocations, OpenFileRequest, Packet,
-//         ReadBlockRequest,
-//     },
-//     errors::CuddlyResult,
-//     APP_CONFIG,
-// };
+use crate::utils::parse_message;
+use crate::{
+    cuddlyproto::{
+        self, file_service_client::FileServiceClient, BlockWithLocations, OpenFileRequest,
+    },
+    errors::CuddlyResult,
+};
 
-// #[allow(dead_code)]
-// pub struct CuddlyReader {
-//     blocks_with_locations: Vec<BlockWithLocations>,
-//     total_file_size: u64,
-//     current_block_offset: u64,
-//     current_block_index: usize,
-//     current_future: Option<Pin<Box<dyn Future<Output = Result<Packet, std::io::Error>> + Send>>>,
-// }
+#[allow(dead_code)]
+pub struct CuddlyReader {
+    blocks_with_locations: Vec<BlockWithLocations>,
+    block_index: usize,
+    total_file_size: u64,
+    current_file_pos: u64,
+    current_reader: BufStream<TcpStream>,
+    current_block_size: u64,
+    current_block_pos: u64,
+    current_packet_size: u64,
+    current_packet_pos: u64,
+    buffer: Vec<u8>,
+    // current_future: Option<Pin<Box<dyn Future<Output = Result<Packet, std::io::Error>> + Send>>>,
+}
 
-// impl CuddlyReader {
-//     pub async fn open(
-//         namenode_rpc_address: String,
-//         file_path: impl Into<String>,
-//     ) -> CuddlyResult<BufReader<Self>> {
-//         let mut namenode_client =
-//             match FileServiceClient::connect(namenode_rpc_address.clone()).await {
-//                 Ok(client) => client,
-//                 Err(e) => {
-//                     return Err(e.into());
-//                 }
-//             };
+impl CuddlyReader {
+    pub async fn open(
+        namenode_rpc_address: String,
+        file_path: impl Into<String>,
+    ) -> CuddlyResult<Self> {
+        let mut namenode_client =
+            match FileServiceClient::connect(namenode_rpc_address.clone()).await {
+                Ok(client) => client,
+                Err(e) => {
+                    return Err(e.into());
+                }
+            };
 
-//         let res = namenode_client
-//             .open_file(OpenFileRequest {
-//                 auth_token: None,
-//                 file_path: file_path.into(),
-//             })
-//             .await?
-//             .into_inner();
+        let res = namenode_client
+            .open_file(OpenFileRequest {
+                auth_token: None,
+                file_path: file_path.into(),
+            })
+            .await?
+            .into_inner();
 
-//         let mut blocks_with_locations = res.blocks_with_locations;
+        let mut blocks_with_locations = res.blocks_with_locations;
 
-//         let total_file_size = blocks_with_locations
-//             .iter()
-//             .fold(0, |acc, b| acc + b.block.as_ref().unwrap().len);
+        let total_file_size = blocks_with_locations
+            .iter()
+            .fold(0, |acc, b| acc + b.block.as_ref().unwrap().len);
 
-//         blocks_with_locations.sort_by(|a, b| {
-//             a.block
-//                 .as_ref()
-//                 .unwrap()
-//                 .seq
-//                 .cmp(&b.block.as_ref().unwrap().seq)
-//         });
+        blocks_with_locations.sort_by(|a, b| {
+            a.block
+                .as_ref()
+                .unwrap()
+                .seq
+                .cmp(&b.block.as_ref().unwrap().seq)
+        });
 
-//         Ok(BufReader::with_capacity(
-//             3 * APP_CONFIG.block_size as usize,
-//             Self {
-//                 blocks_with_locations,
-//                 total_file_size,
-//                 current_block_offset: 0,
-//                 current_block_index: 0,
-//                 current_future: None,
-//             },
-//         ))
-//     }
-// }
+        let current_block = blocks_with_locations[0].block.as_ref().unwrap().clone();
+        let mut current_reader =
+            BufStream::new(TcpStream::connect(&blocks_with_locations[0].locations[0]).await?);
+
+        let mut buffer = vec![];
+        let op = cuddlyproto::Operation {
+            op: cuddlyproto::operation::OpCode::ReadBlock as i32,
+        };
+        op.encode_length_delimited(&mut buffer)?;
+        current_reader.write_all(&buffer).await?;
+        buffer.clear();
+
+        let read_op = cuddlyproto::ReadBlockOperation {
+            block: Some(current_block.clone()),
+        };
+        read_op.encode_length_delimited(&mut buffer)?;
+        current_reader.write_all(&buffer).await?;
+        current_reader.flush().await?;
+
+        Ok(Self {
+            blocks_with_locations,
+            block_index: 0,
+            total_file_size,
+            current_file_pos: 0,
+            current_block_size: current_block.len,
+            current_packet_size: 0,
+            current_block_pos: 0,
+            current_packet_pos: 0,
+            current_reader,
+            buffer: Vec::new(),
+        })
+    }
+
+    pub async fn read(&mut self, buf: &mut [u8]) -> CuddlyResult<usize> {
+        if self.current_file_pos == self.total_file_size {
+            return Ok(0);
+        } else if self.current_block_pos == self.current_block_size {
+            self.next_block().await?;
+        } else if self.current_packet_pos == self.current_packet_size {
+            self.next_packet().await?;
+        }
+
+        let buffer_start_pos = self.current_packet_pos as usize;
+        let bytes_read = Read::read(&mut (&self.buffer[buffer_start_pos..]), buf)? as u64;
+        self.current_file_pos += bytes_read;
+        self.current_block_pos += bytes_read;
+        self.current_packet_pos += bytes_read;
+
+        Ok(bytes_read as usize)
+    }
+
+    pub async fn next_block(&mut self) -> CuddlyResult<()> {
+        self.block_index += 1;
+        let current_block = self.blocks_with_locations[self.block_index]
+            .block
+            .as_ref()
+            .unwrap();
+
+        self.current_block_size = current_block.len;
+        self.current_block_pos = 0;
+
+        let mut current_reader = BufStream::new(
+            TcpStream::connect(&self.blocks_with_locations[self.block_index].locations[0]).await?,
+        );
+        self.buffer.clear();
+
+        let op = cuddlyproto::Operation {
+            op: cuddlyproto::operation::OpCode::ReadBlock as i32,
+        };
+        op.encode_length_delimited(&mut self.buffer)?;
+        current_reader.write_all(&self.buffer).await?;
+        self.buffer.clear();
+
+        let read_op = cuddlyproto::ReadBlockOperation {
+            block: Some(current_block.clone()),
+        };
+        read_op.encode_length_delimited(&mut self.buffer)?;
+
+        current_reader.write_all(&self.buffer).await?;
+        current_reader.flush().await?;
+
+        self.current_reader = current_reader;
+        self.next_packet().await?;
+
+        Ok(())
+    }
+
+    pub async fn next_packet(&mut self) -> CuddlyResult<()> {
+        let cuddlyproto::Packet { size, last: _ } =
+            parse_message::<cuddlyproto::Packet>(&mut self.current_reader).await?;
+        self.current_packet_pos = 0;
+        self.current_packet_size = size;
+        self.buffer.clear();
+        self.buffer.resize_with(size as usize, u8::default);
+        self.current_reader.read_exact(&mut self.buffer).await?;
+        Ok(())
+    }
+}
 
 // impl AsyncRead for CuddlyReader {
 //     fn poll_read(
