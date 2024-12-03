f4837181a6c941639e948e5293594b4c3a29c0df
Niraj Kumar <nirajk23523@gmail.com>
2024-12-03 21:06:55 +0530

    for now

--- src/io/cuddly_writer.rs
+++ src/io/cuddly_writer.rs
@@ -5,22 +5,22 @@ pub struct CuddlyWriter {}
 impl AsyncWrite for CuddlyWriter {
     fn poll_write(
         self: std::pin::Pin<&mut Self>,
-        cx: &mut std::task::Context<'_>,
-        buf: &[u8],
+        _cx: &mut std::task::Context<'_>,
+        _buf: &[u8],
     ) -> std::task::Poll<Result<usize, std::io::Error>> {
         todo!()
     }
 
     fn poll_flush(
         self: std::pin::Pin<&mut Self>,
-        cx: &mut std::task::Context<'_>,
+        _cx: &mut std::task::Context<'_>,
     ) -> std::task::Poll<Result<(), std::io::Error>> {
         todo!()
     }
 
     fn poll_shutdown(
         self: std::pin::Pin<&mut Self>,
-        cx: &mut std::task::Context<'_>,
+        _cx: &mut std::task::Context<'_>,
     ) -> std::task::Poll<Result<(), std::io::Error>> {
         todo!()
     }
